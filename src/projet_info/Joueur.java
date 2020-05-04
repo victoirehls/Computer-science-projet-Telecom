1b6b627965710adbfee18f5d8166c5b5943d5e95
Maiween2000 <63784385+Maiween2000@users.noreply.github.com>
2020-05-04 17:30:57 +0200

    Update Joueur.java

--- src/projet_info/Joueur.java
+++ src/projet_info/Joueur.java
@@ -1,23 +1,5 @@
 package projet_info;
 
-public class Personnage extends Unite {
-	String nom;
-	int points_conquetes;
-	double experience; //en pourcentage
-	int niveau; // entier compris entre 1 et 3
-	char[] territoires;// on mettra un tableau extensible par la suite
-	
-	public Personnage(String nom) {
-		this.nom=nom;
-		this.points_conquetes=0; //on initialise tout à 0 pour l'instant
-		this.niveau=0;
-		this.experience=0;
-	 
-	}
-
-}
-
-
 //DEFINITION DES CHAMPS
 public class Joueur extends Unite{
 	String nom;
