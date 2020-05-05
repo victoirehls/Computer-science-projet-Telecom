6e0bc225ad695a6aa6ee6fca38b4dfaadb1d76ee
Othy99 <63783557+Othy99@users.noreply.github.com>
2020-05-05 20:27:48 +0200

    Update Astre.java

--- src/projet_info/Astre.java
+++ src/projet_info/Astre.java
@@ -8,40 +8,47 @@ public class Astre extends Unite {
 	int coordX;
 	int coordY;
 	int points_conquetes;
-	boolean conquis; //statut de l'astre déterminant si le territoire est conquis
-	int proportion_conquete; //facteur proportionnel qui donne le nombre de points_conquêtes en fonction de sa taille
-	int tour_conquete; //compteur qui marque le tour de conquête d'un astre
-	int taux_croissance; //taux de croissance de population
-	int population;
+	boolean conquis; //statut de l'astre dÃ©terminant si le territoire est conquis
+	int proportion_conquete; //facteur proportionnel qui donne le nombre de points_conquÃªtes en fonction de sa taille
+	int tour_conquete; //compteur qui marque le tour de conquÃªte d'un astre
+	double taux_croissance; //taux de croissance de population
+	double population;
 	int population_max;
 
 //CONSTRUCTEUR
-	public Astre(String nom, int taille, int x, int y, int proportion, int taux) {
+	public Astre(String nom, int taille, int x, int y, int proportion, double taux) {
 		this.nom = nom;
 		this.taille = taille;
 		this.coordX = x;
 		this.coordY = y;
 		this.proportion_conquete = proportion;
-		this.points_conquetes = taille*proportion;
+		this.points_conquetes = taille *proportion;
 		this.conquis = false;
 		this.taux_croissance = taux;
 		this.population_max = 1000*this.taille; //1000 arbitraire
 	}
 	
 //METHODES
-	public void conquerir_astre(Astre a) {
-		a.conquis = true;
-		a.tour_conquete = tour; //tour défini dans la classe joueur : à chaque action d'un joueur tour+=1
-	}
 	
-	public void population(Astre a, Joueur j) { //appelée quand un joueur conquiert un astre
+	
+	public void population(Astre a, Joueur j) { //appelÃ©e quand un joueur conquiert un astre
 		a.population = 1;  //initialisation
 		if(a.population <= a.population_max) {
 			a.population = j.tour*a.taille*a.taux_croissance;
 		}
 		else{
-			a.population = a.population_max;
+			a.population = a.population_max;}
 		}
-	}
 	
+	public String[] displayastre(Astre a ){
+		String tab[] = new String[7];
+		tab[0] = "nom : " + a.nom;
+		tab[1] = " taille : " + String.valueOf(a.taille);
+		tab[2] = "population : " + String.valueOf(a.population);
+		tab[3] = "population max : " + String.valueOf(a.population_max);
+		tab[4] = "coordX : " + String.valueOf(a.coordX);
+		tab[5] = "coordY : " + String.valueOf(a.coordY);
+		tab[6] = "point conquetes : " + String.valueOf(a.points_conquetes);
+		return tab;
+}
 }
