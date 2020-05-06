39df83470eb327c80801269ba65bd784885369fd
victoirehls <63715202+victoirehls@users.noreply.github.com>
2020-05-06 09:26:29 +0200

    Update Case.java

--- src/projet_info/Case.java
+++ src/projet_info/Case.java
@@ -1,4 +1,3 @@
-
 package projet_info;
 
 import java.util.ArrayList;
@@ -7,10 +6,16 @@ import java.util.List;
 public class Case  {
 	List<Unite> unites;
 	
+	
 
 	Case() {
 		this.unites = new ArrayList<Unite>();
-    }
+		
+		
+	}
+	
+	void ajouterUnite(Unite unite) {
+		this.unites.add(unite);
+	}
 	
 }
-
