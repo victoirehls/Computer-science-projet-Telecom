2149442b1ecec2bfbde99f7a64e0836f97d8db2e
Danto1 <63782076+Danto1@users.noreply.github.com>
2020-05-11 14:12:24 +0200

    Update Unite.java

--- src/projet_info/Unite.java
+++ src/projet_info/Unite.java
@@ -1,8 +1,42 @@
 package projet_info;
 
-public abstract class Unite {
+import java.awt.Image;
+import javax.swing.ImageIcon;
 
+public abstract class Unite {
+	//*** VARIABLES ***//
+	private int x,y ;
+	private int largeur, hauteur;
+	
+	protected Image imgObj;
+	protected ImageIcon icoObj;
+	
+	//*** CONSTRUCTEUR ***//
+	public Unite(int x,int y, int largeur, int hauteur) {
+		this.x = x;
+		this.y = y;
+		this.largeur = largeur;
+		this.hauteur = hauteur;
+	}
+
+	//*** GETTER ***//
+	@SuppressWarnings("exports")
+	public Image getImgObj() {return imgObj;}
+	public int getX() {return x;}
+	public int getY() {return y;}
+	
+	
+	//*** METHODES ***//
+	public void deplacement(){
+		if (Main.scene.getxFond() != 0 || Main.scene.getyFond() != 0 ) {
+				this.x += Main.scene.getDx();
+				this.y += Main.scene.getDy();
+		}
+	}
+	
 }
+
+
 
 
 
