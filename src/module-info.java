2be079c912dab2f86759c4c636ad54f681a84d92
Danto1 <63782076+Danto1@users.noreply.github.com>
2020-05-11 14:15:56 +0200

    Update module-info.java

--- src/module-info.java
+++ src/module-info.java
@@ -1,2 +1,12 @@
 module projetinfo {
+  requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.swing;
+	requires javafx.swt;
+	requires javafx.web;
+	requires java.desktop;
+	exports projet_info;
 }
