d8e2f0a8d776a2a2ddd2210475ee32c53a32dad5
Danto1 <63782076+Danto1@users.noreply.github.com>
2020-06-09 12:11:33 +0200

    Update Joueur.java

--- src/projet_info/Joueur.java
+++ src/projet_info/Joueur.java
@@ -12,23 +12,24 @@ public class Joueur extends Unite{
 		int points_conquetes;
 		double experience; //en pourcentage
 		int niveau; // entier compris entre 1 et 3
-		public ArrayList<Astre> territoires;// tableau extensible qui stocke les astres conquis par le joueur
+		public ArrayList<Astre> territoires;// on mettra un tableau extensible par la suite
 		int tour; //compte le nombre de tours de la partie
 		//double[] coordonnees;
 		int points_dactions;
 		int main;
+		int numero;
 		
 
-		//private Image imgJoueur;
-		//private ImageIcon icoJoueur;
-		
-
+		private Image imgObj1;
+		private ImageIcon icoObj1;
+		private Image imgObj2;
+		private ImageIcon icoObj2;
 		
 	//CONSTRUCTEUR
-		public Joueur(String nom, int x, int y) { //public Joueur(String nom, int x, int y, super.icoObj 
+		public Joueur(String nom, int x, int y, int numero) {
 			super(x,y,30,30);
 			this.nom = nom;
-			this.points_conquetes = 0; //on initialise tout Ã  0 pour l'instant
+			this.points_conquetes = 1000; //on initialise tout Ã  0 pour l'instant
 			this.niveau = 1;
 			this.experience = 0;
 			this.tour=1;
@@ -36,9 +37,16 @@ public class Joueur extends Unite{
 			//this.coordonnees[0] = x; //initialisation des coordonnées de chaque joueur
 			//this.coordonnees[1] = y;
 			//this.territoires = new String[100]; // on prend un "grand " tableau pour l'instant pour Ã©viter d'utiliser les tableaux exte
-			super.icoObj = new ImageIcon(getClass().getResource("joueur.png")); 
+			super.icoObj = new ImageIcon(getClass().getResource("joueur3.png")); 
 			super.imgObj = this.icoObj.getImage(); 
 			this.territoires = new ArrayList<Astre>();
+			this.numero = 0;
+			
+			icoObj1 = new ImageIcon(getClass().getResource("soleil4.png")); //
+			imgObj1 = this.icoObj1.getImage(); //
+			icoObj2 = new ImageIcon(getClass().getResource("soleil5.png")); //
+			imgObj2 = this.icoObj1.getImage(); //
+			
 		}
 	
 	//GETTERS ET SETTERS
@@ -85,20 +93,13 @@ public class Joueur extends Unite{
 		public void setTerritoires(ArrayList<Astre> territoires) {
 			this.territoires = territoires;
 		}
+		
 
 		//METHODES
-		public static int getTaille() {
-			int taille_totale = 0;
-			for(int i =0; i<= Scene.tabObjets.size(); i++){
-				taille_totale = taille_totale + Scene.tabObjets.get(i).getTaille();
-					}
-			return taille_totale;	
-		}
 	public static void passer_tour(Joueur joueurquijoue, Joueur autre) { // Ã  appeler Ã  chaque tour d'un seul joueur
 			joueurquijoue.tour += 1;
 			joueurquijoue.main = 0;
 			autre.main = 1;
-			joueurquijoue.points_conquetes += joueurquijoue.getTaille()*100;
 			if (joueurquijoue.niveau == 1) { joueurquijoue.points_dactions = 10;}
 			else if (joueurquijoue.niveau == 2) { joueurquijoue.points_dactions = 20;}
 			else if (joueurquijoue.niveau == 3) { joueurquijoue.points_dactions = 30;}
@@ -135,7 +136,7 @@ public class Joueur extends Unite{
 			return m;*/
 		}
 		
-		public void conquerir(Astre a) {
+		public void conquerir(Astre a, Joueur j) {
 			if (this.main == 1) {
 				if (this.points_dactions >= 0) {
 					//if (j.points_conquetes >= a.points_conquetes)
@@ -145,7 +146,12 @@ public class Joueur extends Unite{
 						this.points_dactions -= 1;
 						this.territoires.add(a); //ca marche pas avec les lignes d'après
 						a.setConquis(true);
-					
+						if(j.numero == 1) 
+							a.setIcoObj(icoObj1);
+							a.setImgObj(imgObj1);
+						if(j.numero == 2) 
+							a.setIcoObj(icoObj2);
+							a.setImgObj(imgObj2);
 						/*int m = compter_territoires (j);
 						a.tour_conquete = j.tour;
 						j.territoires[m] = (a.nom);
@@ -157,6 +163,7 @@ public class Joueur extends Unite{
 						j.points_dactions -= 5;*/
 				}
 			}
+		}
 					//else {System.out.println("Vous n'avez pas assez de points concquêtes");
 					//} }
 				//else {System.out.println("Vous n'avez pas assez de points d'actions");
@@ -164,20 +171,20 @@ public class Joueur extends Unite{
 			//else {System.out.println("Ce n'est pas encore votre tour");
 				//}	
 			
-		}
+	
 		
-		public void attaquer(Joueur j2, Astre a) {
+		public void attaquer(Joueur j, Astre a) {
 			if (this.main == 1) {
 				if (this.points_dactions >= 5) {
-					System.out.println("Le joueur" + this.nom + " attaque l'astre " + a.nom + "de" + j2.nom);
+					System.out.println("Le joueur" + this.nom + " attaque l'astre " + a.nom + "de" + j.nom);
 					if ((this.points_conquetes >= a.points_conquetes) && (a.getX() == this.getX()) && (a.getY() == this.getY())) {
-						this.conquerir(a);
-						j2.territoires.remove(a); // remplace les deux lignes qui suivent
+						this.conquerir(a,j);
+						j.territoires.remove(a); // remplace les deux lignes qui suivent
 						//for(int i =0; i<= j2.territoires.size(); i++){ 
 							//if (j2.territoires.get(i) == a) { 
 								
 					}
-							System.out.println("Le joueur" + this.nom + " a conquit l'astre " + a.nom + "de" + j2.nom);
+							System.out.println("Le joueur" + this.nom + " a conquit l'astre " + a.nom + "de" + j.nom);
 			
 						}
 						this.points_dactions -= 7;
@@ -185,7 +192,7 @@ public class Joueur extends Unite{
 				
 			
 					else {
-						System.out.println("Le joueur" + this.nom + " n'a pas assez de points conquêtes pour conquérir l'astre " + a.nom + "de" + j2.nom + " en plus il est moche ");
+						System.out.println("Le joueur" + this.nom + " n'a pas assez de points conquêtes pour conquérir l'astre " + a.nom + "de" + j.nom + " en plus il est moche ");
 				
 			}}
 				//else {System.out.println("Vous n'avez pas assez de points d'actions"); }}
